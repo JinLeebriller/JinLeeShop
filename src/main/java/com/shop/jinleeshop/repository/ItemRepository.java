297ec4ed1f1e25ae6b18ef73cb42d20e19987de9
JinLeebriller <boomteru@naver.com>
2023-10-09 16:05:52 +0900

    JinLeeShop

--- src/main/java/com/shop/jinleeshop/repository/ItemRepository.java
+++ src/main/java/com/shop/jinleeshop/repository/ItemRepository.java
@@ -3,5 +3,10 @@ package com.shop.jinleeshop.repository;
 import com.shop.jinleeshop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
 }
