02798767ac58a3b7df1d82956005f0f932198f59
JinLeebriller <boomteru@naver.com>
2023-10-25 22:05:40 +0900

    JinLeeShop

--- src/main/java/com/shop/jinleeshop/entity/Item.java
+++ src/main/java/com/shop/jinleeshop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.shop.jinleeshop.entity;
 
 import com.shop.jinleeshop.constant.ItemSellStatus;
+import com.shop.jinleeshop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -50,5 +51,13 @@ public class Item extends BaseEntity {
     // 수정 시간
     private LocalDateTime updateTime;
     */
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 
 }
--- src/main/java/com/shop/jinleeshop/service/ItemImgService.java
+++ src/main/java/com/shop/jinleeshop/service/ItemImgService.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -50,6 +52,31 @@ public class ItemImgService {
             */
             itemImg.updateItemImg(oriImgName, imgName, imgUrl);
             itemImgRepository.save(itemImg);
+        }
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
+        // 상품 이미지를 수정한 후 업데이트
+        if(!itemImgFile.isEmpty()) {
+            // 상품 이미지 아이디를 이용하여 기존에 저장했던 상품 이미지 엔티티를 조회
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+            // 기존 이미지 파일 삭제
+            if(!StringUtils.isEmpty(savedItemImg.getImgName())) {
+                fileService.deleteFile(itemImgLocation + "/" + savedItemImg.getImgName());
+            }
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            // 업데이트한 상품 이미지 파일을 업로드
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            String imgUrl = "/images/item/" + imgName;
+            /*
+                변경된 상품 이미지 정보를 세팅.
+                상품 등록 때처럼 itemImgRepository.save() 로직을 호출하지 않는 이유는
+                savedItemImg 엔티티는 현재 영속 상태이므로 데이터를 변경하는 것만으로 변경 감지 기능이 동작하여 트랜잭션이 끝날 때 updata 쿼리가 실행
+                (중요한 것은 엔티티가 영속 상태여야 한다는 것)
+            */
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
         }
     }
 
