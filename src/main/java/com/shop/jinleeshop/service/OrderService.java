3534344db50a8915ead5ce5d34a5471532f311ff
JinLeebriller <boomteru@naver.com>
2023-11-05 16:48:27 +0900

    JinLeeShop

--- src/main/java/com/shop/jinleeshop/service/OrderService.java
+++ src/main/java/com/shop/jinleeshop/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shop.jinleeshop.service;
 
 import com.shop.jinleeshop.dto.OrderDto;
-import com.shop.jinleeshop.entity.Item;
-import com.shop.jinleeshop.entity.Member;
-import com.shop.jinleeshop.entity.Order;
-import com.shop.jinleeshop.entity.OrderItem;
+import com.shop.jinleeshop.dto.OrderHistDto;
+import com.shop.jinleeshop.dto.OrderItemDto;
+import com.shop.jinleeshop.entity.*;
+import com.shop.jinleeshop.repository.ItemImgRepository;
 import com.shop.jinleeshop.repository.ItemRepository;
 import com.shop.jinleeshop.repository.MemberRepository;
 import com.shop.jinleeshop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +31,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         // 주문할 상품을 조회
@@ -46,6 +50,34 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    // 주문 목록을 조회하는 로직
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+
+        // 유저의 아이디와 페이징 조건을 이용하여 주문 목록을 조회
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        // 유저의 주문 총 개수를 조회
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        // 주문 리스트를 순회하면서 구매 이력 페이지에 전달할 DTO를 생성
+        for(Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for(OrderItem orderItem : orderItems) {
+                // 주문한 상품의 대표 이미지를 조회
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+            orderHistDtos.add(orderHistDto);
+        }
+
+        // 페이지 구현 객체를 생성하여 반환
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
 
 }
