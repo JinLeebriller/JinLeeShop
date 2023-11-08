d745b644289b2444a73f90f3c37d6119df7149cb
JinLeebriller <boomteru@naver.com>
2023-11-08 14:57:16 +0900

    JinLeeShop

---
+++ src/main/java/com/shop/jinleeshop/controller/CartContoller.java
@@ -0,0 +1,9 @@
+package com.shop.jinleeshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class CartContoller {
+}
